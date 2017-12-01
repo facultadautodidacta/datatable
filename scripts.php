558c74a78579d66f6204f0bc7bcbe0559bfc44a5
Roldan Aquino Segura <roldanaquinosegura@gmail.com>
2017-12-01 10:48:15 -0600

    Estructura de carpetas y agregado de dependencias

---
+++ scripts.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" type="text/css" href="librerias/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="librerias/datatable/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="librerias/datatable/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" type="text/css" href="librerias/alertify/css/alertify.css">
+<link rel="stylesheet" type="text/css" href="librerias/alertify/css/themes/bootstrap.css">
+
+<script src="librerias/jquery.min.js"></script>
+<script src="librerias/bootstrap/popper.min.js"></script>
+<script src="librerias/bootstrap/bootstrap.min.js"></script>
+<script src="librerias/datatable/jquery.dataTables.min.js"></script>
+<script src="librerias/datatable/dataTables.bootstrap4.min.js"></script>
+<script src="librerias/alertify/alertify.js"></script>
